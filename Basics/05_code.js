d60018d81b03d399529a4df30b89fd5adb4b2404
Dheeraj6392 <154231060+Dheeraj6392@users.noreply.github.com>
2024-08-26 05:27:21 +0000

    container config

---
+++ Basics/05_code.js
@@ -0,0 +1,25 @@
+console.log("Dheeraj");
+console.log("Hey");
+const account_Id = 1444533;
+let accountEmail = "DheerajSonkar";
+var accountPassword = "134";
+// let accountCity;
+
+accountCity = "Jaipur";
+console.table([account_Id, accountEmail, accountPassword, accountCity]);
+
+"use strict";
+// alert(3 + 3);     // we are using nodejs not browser
+ 
+console.log( 3 + 3);
+let name = "Dheeraj";
+let bool = true;
+
+// null =>   standalone value;
+// undefind => 
+// symbol   => unique
+
+// object 
+
+console.log(typeof(name));
+console.log(typeof(null));
